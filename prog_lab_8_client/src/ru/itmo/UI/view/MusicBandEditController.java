ec7e983ad3a52e2b1f7411f27daf6277b1d65d2e
Maxim <mamode12@yandex.com>
2020-08-05 18:16:56 +0300

    Panes' controller classes are created (no business-logic inside at the moment).

---
+++ prog_lab_8_client/src/ru/itmo/UI/view/MusicBandEditController.java
@@ -0,0 +1,14 @@
+package ru.itmo.UI.view;
+
+import ru.itmo.UI.Main;
+
+public class MusicBandEditController {
+
+    Main main;
+
+
+
+    public void setMain(Main main) {
+        this.main = main;
+    }
+}
---
+++ prog_lab_8_client/src/ru/itmo/UI/view/RootPaneController.java
@@ -0,0 +1,16 @@
+package ru.itmo.UI.view;
+
+import ru.itmo.UI.Main;
+
+public class RootPaneController {
+
+    Main main;
+
+
+
+
+
+    public void setMain(Main main) {
+        this.main = main;
+    }
+}
